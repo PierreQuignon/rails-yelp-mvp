609351c78c4bfade1b23cdcc09d58d1fc3a9a213
Pierre Quignon <p.quignon.pro@gmail.com>
2022-11-16 16:56:12 +0100

    commit exo2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "creating restaurants"
+
+Restaurant.create({
+  name:'La Bodega',
+  address:'2 rue mabilais',
+  phone_number:'06.20.30.40.50',
+  category:'french'
+})
+
+Restaurant.create({
+  name:'Japanfood',
+  address:'3 rue beaurepaire',
+  phone_number:'06.20.70.90.80',
+  category:'japanese'
+})
+
+Restaurant.create({
+  name:'Roma',
+  address:'3 rue pigeon',
+  phone_number:'06.20.70.80.20',
+  category:'italian'
+})
+
+Restaurant.create({
+  name:'Chinafood',
+  address:'3 rue wagon',
+  phone_number:'06.99.88.33.20',
+  category:'japanese'
+})
+
+Restaurant.create({
+  name:'Saveurs de Savoie',
+  address:'5 avenue Charle Degaule',
+  phone_number:'06.20.77.33.99',
+  category:'french'
+})
+
+puts "finish"
